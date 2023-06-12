a99bc3a71675d6ffca78511cd2f71026a2a82f35
khkh0109 <ist200120@gmail.com>
2023-06-12 11:49:50 +0900

    refactor: 이메일, 비밀번호를 Input 컴포넌트로 분리 (#1)

---
+++ src/components/Input.jsx
@@ -0,0 +1,18 @@
+function Input({ label, type, id, testid, setValue }) {
+  return (
+    <>
+      <label htmlFor={id}>{label}</label>
+      <input
+        type={type}
+        id={id}
+        data-testid={testid}
+        required
+        onChange={e => {
+          setValue(e.target.value);
+        }}
+      />
+    </>
+  );
+}
+
+export default Input;
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import Input from "../components/Input";
 
 function Signup() {
   const [email, setEmail] = useState("");
@@ -8,62 +9,55 @@ function Signup() {
   const navigate = useNavigate();
 
   return (
-    <>
-      <form>
-        <label htmlFor="email">이메일</label>
-        <input
-          type="text"
-          id="email"
-          data-testid="email-input"
-          required
-          onChange={e => {
-            setEmail(e.target.value);
-          }}
-        />
+    <form>
+      <Input
+        label="이메일"
+        type="text"
+        id="email"
+        testid="email-input"
+        setValue={setEmail}
+      />
 
-        <label htmlFor="password">비밀번호</label>
-        <input
-          type="password"
-          id="password"
-          data-testid="password-input"
-          required
-          onChange={e => {
-            setPassword(e.target.value);
-          }}
-        />
+      <Input
+        label="비밀번호"
+        type="password"
+        id="password"
+        testid="password-input"
+        setValue={setPassword}
+      />
 
-        <button
-          type="submit"
-          data-testid="signup-button"
-          disabled={
-            email && email.includes("@") && password.length >= 8 ? false : true
-          }
-          onClick={e => {
-            e.preventDefault();
-            axios
-              .post(
-                `${process.env.REACT_APP_BASE_URL}/auth/signup`,
-                { email, password },
-                {
-                  headers: {
-                    "Content-Type": "application/json",
-                  },
-                }
-              )
-              .then(() => {
-                return navigate("/signin");
-              })
-              .catch(err => {
-                if (err.response.data.statusCode === 400) {
-                  console.log(err.response.data.message);
-                }
-              });
-          }}
-        >
-          회원가입
-        </button>
-      </form>
-    </>
+      <button
+        type="submit"
+        data-testid="signup-button"
+        disabled={
+          email && email.includes("@") && password.length >= 8 ? false : true
+        }
+        onClick={e => {
+          e.preventDefault();
+          axios
+            .post(
+              `${process.env.REACT_APP_BASE_URL}/auth/signup`,
+              { email, password },
+              {
+                headers: {
+                  "Content-Type": "application/json",
+                },
+              }
+            )
+            .then(() => {
+              alert("성공적으로 가입되었습니다.");
+              return navigate("/signin");
+            })
+            .catch(err => {
+              if (err.response.data.statusCode === 400) {
+                console.log(err.response.data.message);
+              }
+            });
+        }}
+      >
+        회원가입
+      </button>
+    </form>
   );
 }
 
