0aac35542f3f401c9a929c230d30fb44f18175e5
khkh0109 <ist200120@gmail.com>
2023-06-12 10:59:36 +0900

    feat: 회원가입 요청 기능 추가 (#1)

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,8 +1,11 @@
 import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 function Signup() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const navigate = useNavigate();
 
   return (
     <>
@@ -35,6 +38,27 @@ function Signup() {
           disabled={
             email && email.includes("@") && password.length >= 8 ? false : true
           }
+          onClick={e => {
+            e.preventDefault();
+            axios
+              .post(
+                `${process.env.REACT_APP_BASE_URL}/auth/signup`,
+                { email, password },
+                {
+                  headers: {
+                    "Content-Type": "application/json",
+                  },
+                }
+              )
+              .then(() => {
+                return navigate("/signin");
+              })
+              .catch(err => {
+                if (err.response.data.statusCode === 400) {
+                  console.log(err.response.data.message);
+                }
+              });
+          }}
         >
           회원가입
         </button>
