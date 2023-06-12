b0576de0abc5fccd349e6a7971df859362c33cf4
khkh0109 <ist200120@gmail.com>
2023-06-12 10:32:48 +0900

    feat: 회원가입 유효성 검사 기능 추가 (#1)

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,5 +1,46 @@
+import { useState } from "react";
+
 function Signup() {
-  return <div>Signup</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <>
+      <form>
+        <label htmlFor="email">이메일</label>
+        <input
+          type="text"
+          id="email"
+          data-testid="email-input"
+          required
+          onChange={e => {
+            setEmail(e.target.value);
+          }}
+        />
+
+        <label htmlFor="password">비밀번호</label>
+        <input
+          type="password"
+          id="password"
+          data-testid="password-input"
+          required
+          onChange={e => {
+            setPassword(e.target.value);
+          }}
+        />
+
+        <button
+          type="submit"
+          data-testid="signup-button"
+          disabled={
+            email && email.includes("@") && password.length >= 8 ? false : true
+          }
+        >
+          회원가입
+        </button>
+      </form>
+    </>
+  );
 }
 
 export default Signup;
