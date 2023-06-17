9b0f2e01058df0714a54ad8f7a4657177221af78
khkh0109 <ist200120@gmail.com>
2023-06-17 17:59:52 +0900

    design: 수정, 삭제 버튼 위치 변경 (#5)

--- src/components/TodoItemButtonGroup.jsx
+++ src/components/TodoItemButtonGroup.jsx
@@ -2,18 +2,18 @@ function TodoItemButtonGroup({ handleDeleteButton, handleModifyButton }) {
   return (
     <div className="join">
       <button
-        data-testid="delete-button"
-        onClick={handleDeleteButton}
-        className="btn btn-sm join-item"
-      >
-        삭제
-      </button>
-      <button
         data-testid="modify-button"
         onClick={handleModifyButton}
         className="btn btn-sm join-item"
       >
         수정
+      </button>
+      <button
+        data-testid="delete-button"
+        onClick={handleDeleteButton}
+        className="btn btn-sm join-item"
+      >
+        삭제
       </button>
     </div>
   );
